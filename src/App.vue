148094ca8c08f8614793b6c52f830a92a2060611
crisyhuang <1205281078@qq.com>
2026-01-08 12:11:45 +0800

    feat: 实现路由功能

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,26 @@
 <template>
-  <template-detail />
+  <div class="homepage-container">
+    <a-layout :style="{ background: '#fff' }">
+      <a-layout-header class="header">
+        <div class="page-title">乐高</div>
+      </a-layout-header>
+      <a-layout-content class="home-layout">
+        <router-view></router-view>
+      </a-layout-content>
+    </a-layout>
+    <a-layout-footer :style="{ textAlign: 'center' }"
+      >© crisyhuang</a-layout-footer
+    >
+  </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
-import TemplateDetail from './views/TemplateDetail.vue';
-export default defineComponent({
-  components: {
-    TemplateDetail,
-  },
-});
+export default defineComponent({});
 </script>
+
+<style>
+.page-title {
+  color: #fff;
+}
+</style>
--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,10 @@
 import { createApp } from 'vue';
-import './style.css';
+// import './style.css';
 import App from './App.vue';
 import Antd from 'ant-design-vue';
 import 'ant-design-vue/dist/reset.css';
+import router from './routes';
 
 const app = createApp(App);
-app.use(Antd);
+app.use(Antd).use(router);
 app.mount('#app');
